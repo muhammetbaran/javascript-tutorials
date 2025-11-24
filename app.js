44401553d327ea7a8431b0a87ed3fa5997d1b87b
muhammetbaran <muhammetaliibaran@gmail.com>
2025-11-24 13:47:21 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+let a = "Muhammet Ali"
+console.log(a)
+document.writeln("ali<br>")
+document.write("test<br>")
+document.write(false)
+// !alert("napıyon la")
+let c=7;
+let b=9;
+console.log("ali baba",c+b)
